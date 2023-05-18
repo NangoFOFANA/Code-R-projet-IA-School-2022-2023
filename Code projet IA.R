316424ee73fc93dc48d4bb103cb5c0012e586f2d
Nango  Fofana <73390726+NangoFOFANA@users.noreply.github.com>
2023-05-18 21:58:06 +0200

    Add files via upload

--- Code projet IA.R
+++ Code projet IA.R
@@ -18,39 +18,39 @@ library(ggcorrplot)
 library(RColorBrewer)
 library(RColorBrewer)
 
-### Importation de la première base de données lié à la matières
-### obtenue après un webscrapping avec octoparse
+### Importation de la premi?re base de donn?es li? ? la mati?res
+### obtenue apr?s un webscrapping avec octoparse
 
-data1 <- read.csv("C:/Users/fofna/OneDrive/Bureau/Dossiers 2022-2023/IA 2022-2023/Projet IA 2022-23/data_2.csv")
+data1 <- read.csv("C:/Users/fofna/OneDrive/Bureau/Dossiers 2022-2023/IA 2022-2023/Projet IA 2022-23/fichier_final.csv")
 
 View(data1) ## pour voir toute la base de donn
-head(data,5) ## voir les cinq première ligneView(data_2)
+head(data,5) ## voir les cinq premi?re ligneView(data_2)
 attach(data1)
 
-## Summary de la base pour faire la statistique de notre base de données
+## Summary de la base pour faire la statistique de notre base de donn?es
 summary(data1)
 
 ## La fonction ci-dessous permet de voir le nombre
-## de matière que nous avons 
-table(matière)
+## de mati?re que nous avons 
+table(matieres)
 
 
-## La fonction ci-dessous permet de graphique de ces matières
+## La fonction ci-dessous permet de graphique de ces mati?res
 x11()
-barp1 <- barplot(table(matière),
-                 main = "Histogramme des matières",
-                 ylab = "Frequence",xlab = "Matières",
+barp1 <- barplot(table(matieres),
+                 main = "Histogramme des mati?res",
+                 ylab = "Frequence",xlab = "Mati?res",
                  col = c("cyan", "beige","pink",
                          "grey","Salmon","turquoise",
                          "purple", "blue", "light green", 
                          "green", "yellow", "gold","light blue",
                          "plum"),ylim=c (0, 150),
                  args.legend = list(x = "top"),
-                 legend.text = rownames(table(matière)))
-text(barp1, table(matière)+ 0.01, labels = table(matière))
+                 legend.text = rownames(table(matieres)))
+text(barp1, table(matieres)+ 0.01, labels = table(matieres))
 
 ## Fonction pour les diagramme en camembert
-pie(table(matière), main = "Diagramme camembert des matières",
+pie(table(matieres), main = "Diagramme camembert des mati?res",
     col = c("cyan", "beige","pink",
             "grey","Salmon","chocolate",
             "purple", "dark blue"))
@@ -59,62 +59,75 @@ pie(table(matière), main = "Diagramme camembert des matières",
 
 ##########################################################
 
-### Importation de la deuxième base de données lié 
-### au colorant utilisée pour colorer les matières des artices
-### obtenue egalement après un webscrapping en utilisant octoparse
-
-### Nous ferons également les même étapes que la première data
-
-data_1 <- read.csv("C:/Users/fofna/OneDrive/Bureau/Dossiers 2022-2023/IA 2022-2023/Projet IA 2022-23/data_1.csv")
-
-attach(data_1)
-View(data_1)
-
-## Summary de la base pour faire la statistique de notre base de données
-summary(data_1)
+### Autre visualisation liÃ© aux colorant utilis?e pour colorer les mati?res des artices
+### obtenue egalement apr?s un webscrapping en utilisant octoparse
 
 ## La fonction ci-dessous permet de voir le repartion
 ## des colorants que nous avons 
-table(produits.de.coloration)
+table(produits_coloration)
 
 ##
-barp2 <- barplot(table(produits.de.coloration),
+barp2 <- barplot(table(produits_coloration),
                 main = "Histogramme produits de coloration",
                 ylab = "Frequence",xlab = "Produits",
                 col = c("cyan", "beige","pink",
                         "grey","Salmon","chocolate",
                         "purple", "dark blue"),ylim=c (0, 150),
                 args.legend = list(x = "topright"),
-                legend.text = rownames(table(produits.de.coloration)))
-text(barp2, table(produits.de.coloration)+ 0.5, 
-     labels = table(produits.de.coloration))
+                legend.text = rownames(table(produits_coloration)))
+text(barp2, table(produits_coloration)+ 0.5, 
+     labels = table(produits_coloration))
 
 ## Fonction pour les diagramme en camembert
-pie(table(produits.de.coloration), main = "Diagramme camembert des matières",
+pie(table(produits.de.coloration), main = "Diagramme camembert des mati?res",
     col = c("dark green","turquoise","orange","red", "light green","green"))
 
 
 ######################################################
-### Importation de la troisième base de données lié 
-### aux fibre utilisée pour es matières des artices
-### obtenue egalement après un webscrapping en utilisant octoparse
+###Autre visualisation liÃ© aux fibre utilis?e pour es mati?res des artices
+### obtenue egalement apr?s un webscrapping en utilisant octoparse
 
-### Nous ferons également les même étapes que la première data
-
-data_3 <- read.csv("C:/Users/fofna/OneDrive/Bureau/Dossiers 2022-2023/IA 2022-2023/Projet IA 2022-23/data_3.csv")
-
-View(data_3)
-attach(data_3)
-summary(data_3)
-table(étape)
+table(etape)
 
 x11()
-barp3 <- barplot(table(étape),
-                main = "Histogramme santé humaine",
-                ylab = "Frequence",xlab = "Les étapes",
+barp3 <- barplot(table(etape),
+                main = "Histogramme sant? humaine",
+                ylab = "Frequence",xlab = "Les ?tapes",
                 col  = c("dark cyan", "dark green", "dark blue",
                          "beige","pink",
                          "grey","chocolate","chocolate"), ylim=c (0, 150),
                 args.legend = list(x = "top"),
-                legend.text = rownames(table(étape)))
-text(barp3, table(étape)+ 0.01, labels = table(étape))
+                legend.text = rownames(table(etape)))
+text(barp3, table(etape)+ 0.01, labels = table(etape))
+
+
+
+##" Le pourcentage moyen par matÃ¨res
+aggregate(x= pourcentage_matieres,     
+          
+          # Specify group indicator
+          by = list(matieres),      
+          
+          # Specify function (i.e. mean)
+          FUN = mean)
+
+##" L'Ã©cart-type par matÃ¨res
+aggregate(x= pourcentage_matieres,     
+          
+          # Specify group indicator
+          by = list(matieres),      
+          
+          # Specify function (i.e. mean)
+          FUN = sd)
+
+
+## 
+
+##" Le pourcentage moyen par produit coloratif
+aggregate(x= pourcentage_matieres,     
+          
+          # Specify group indicator
+          by = list(produits_coloration),      
+          
+          # Specify function (i.e. mean)
+          FUN = mean)
